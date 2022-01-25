dc31ccdaf3ad46b44eb34c047987b225874ae546
limajeferson <jefersonlima@outlook.com>
2022-01-24 23:18:14 -0300

    Update jogo-americano.dart

--- bin/jogo-americano.dart
+++ bin/jogo-americano.dart
@@ -44,17 +44,26 @@ void processLine(String line) {
 
 /* 
 * CONSIDERAÇÕES *
-Num jogo de futebol entre amigos é muito comum que ninguém queira ser o goleiro. Para resolver esse impasse, um time decidiu utilizar o “Jogo Americano".
+Num jogo de futebol entre amigos é muito comum que ninguém queira ser o 
+goleiro. Para resolver esse impasse, um time decidiu utilizar o 
+“Jogo Americano".
 
-Neste jogo, o time faz uma roda e cada um dos jogadores "joga" um número X qualquer. Após isso, o capitão do time soma todos os números jogados e obtém o resultado R.
+Neste jogo, o time faz uma roda e cada um dos jogadores "joga" um número X 
+qualquer. Após isso, o capitão do time soma todos os números jogados e obtém 
+o resultado R.
 
-Depois, começa a contar de 1 até R apontando inicialmente para si mesmo e falando alto e claramente "um", depois para o jogador à sua direita e falando "dois" e assim por diante. O goleiro será aquele que estiver sendo apontado quando o capitão chegar a R.
+Depois, começa a contar de 1 até R apontando inicialmente para si mesmo e 
+falando alto e claramente "um", depois para o jogador à sua direita e falando 
+"dois" e assim por diante. O goleiro será aquele que estiver sendo apontado 
+quando o capitão chegar a R.
 
-1. Escreva um algoritmo que receba uma lista contendo o nome e o número do jogador.
+1. Escreva um algoritmo que receba uma lista contendo o nome e o número do 
+jogador.
 2. O número de jogadores pode variar entre 5 e 11.
 3. Some todos os números informados pelos jogadores.
 4. Faça a contagem dos jogadores, de 1 até o total somado (R).
-5. Se R for maior que a quantidade de jogadores, “dê mais uma volta” começando pelo capitão do time novamente, até alcançar R.
+5. Se R for maior que a quantidade de jogadores, “dê mais uma volta” começando 
+pelo capitão do time novamente, até alcançar R.
 6. Imprima na tela o nome do goleiro.
 Input Format
 
